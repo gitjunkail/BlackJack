0145a4fcdbfea5cd059ec83e5aca4f0f020487eb
gitjunkail <42134925+gitjunkail@users.noreply.github.com>
2019-04-20 17:29:00 -0700

    Update BlackJack.cpp

--- BlackJack.cpp
+++ BlackJack.cpp
@@ -1,5 +1,45 @@
 #include <iostream>
+#include <ctype.h>
 #include <time.h>
+
+int Dealer(int dealer_total)
+{
+	int dealer = rand() % 11 + 1;
+	dealer_total = dealer + dealer_total;
+	std::cout << "dealer " << dealer_total << "\n";
+
+	return dealer_total;
+}
+
+int Player(int player_total)
+{
+	int player = rand() % 11 + 1;
+	player_total = player + player_total;
+	std::cout << "player " << player_total << "\n";
+
+	return player_total;
+}
+
+void EndGameCalculation(int player_total, int dealer_total)
+{
+
+	if (player_total > 21) //dealer doesnt bust
+	{
+		std::cout << "Bust!\n";
+	}
+	else if (player_total > dealer_total)
+	{
+		std::cout << "Player Wins!\n";
+	}
+	else if (dealer_total > player_total)
+	{
+		std::cout << "Dealer Wins!\n";
+	}
+	else if (player_total = dealer_total)
+	{
+		std::cout << "Tie!\n";
+	}
+}
 
 //dealer rule: always have a minimum of 17
 int main()
@@ -8,51 +48,26 @@ int main()
 	srand((unsigned int)time(NULL)); //static_cast<unsigned int>(time(NULL))
 
     std::cout << "Black Jack Game\n"; 
-	int dealer = 0, dealer_total = 0;
-	int player = 0, player_total = 0;
-	char player_continue = ' ';
+	int dealer_total = 0;
+	int player_total = 0;
+	char player_continue = 'y';
 
 
-	dealer = rand() % 11 + 1;
-	dealer_total = dealer + dealer_total;
-	std::cout << "dealer " << dealer_total << "\n";
-
-	player = rand() % 11 + 1;
-	player_total = player + player_total;
-	std::cout << "player " << player_total << "\n";
-
-
-	std::cout << "continue? ";
-	std::cin >> player_continue;
 	while (player_continue == 'y')
 	{
-		player = rand() % 11 + 1;
-		player_total = player + player_total;
-		std::cout << "player " << player_total << "\n";
+		player_total = Player(player_total);
 
-		std::cout << "continue? ";
+		std::cout << "deal another card? Y or N? ";
 		std::cin >> player_continue;
+		player_continue = tolower(player_continue);
 	}
 
 	while (dealer_total < 17)
 	{
-		dealer = rand() % 11 + 1;
-		dealer_total = dealer + dealer_total;
-		std::cout << "dealer " << dealer_total << "\n";
+		dealer_total = Dealer(dealer_total);
 	}
 
-	if (player_total > 21)
-	{
-		std::cout << "bust!\n";
-	}
-	else if (player_total > dealer_total)
-	{
-		std::cout << "player win!\n";
-	}
-	else if (dealer_total > player_total)
-	{
-		std::cout << "dealer win!\n";
-	}
+	EndGameCalculation(player_total, dealer_total);
 
 	return 0;
 }
